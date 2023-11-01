dbb2c464c63fbedcb9f8a8d280e04ebbafd913c6
Ruksana <ruksana01official@gmail.com>
2023-11-01 17:43:05 +0600

    first commit

---
+++ src/Routers/MainRouter.jsx
@@ -0,0 +1,13 @@
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+
+
+const MainRouter = createBrowserRouter([
+    {
+        path: '/',
+        element: <div>hi</div>
+    }
+])
+
+export default MainRouter;
